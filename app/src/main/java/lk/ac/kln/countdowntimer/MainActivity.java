7f0479f034e935a911be544c1f5ea2702a1fc8c0
malkiparanawithana <anjmalankavin@gmail.com>
2019-03-06 14:37:46 +0530

    Added a Button and a text view
    Implemented a CountDown Timer

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -1,13 +1,32 @@
 package lk.ac.kln.countdowntimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int Counter = 99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CountDown();
     }
+
+     private void  CountDown () {
+         final TextView textView = findViewById(R.id.textView);
+         final Handler handler = new Handler();
+         handler.post(new Runnable() {
+             @Override
+             public void run() {
+                 textView.setText(Integer.toString(Counter));
+                 Counter--;
+                 handler.postDelayed(this, 1000);
+
+             }
+         });
+
+     }
 }
+
