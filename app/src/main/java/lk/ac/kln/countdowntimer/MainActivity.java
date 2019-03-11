d8c6db06ad931d0aea16ae1dbffcadee1140adf2
malkiparanawithana <anjmalankavin@gmail.com>
2019-03-11 09:22:33 +0530

    Countdown Timer

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -6,22 +6,39 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int Counter = 99;
+    private int counter = 99;
+    private static final String CURRENT_COUNTER = "counter";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(CURRENT_COUNTER);
+        }
         CountDown();
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(CURRENT_COUNTER,counter);
+    }
+
+
+
      private void  CountDown () {
-         final TextView textView = findViewById(R.id.textView);
+
          final Handler handler = new Handler();
          handler.post(new Runnable() {
              @Override
              public void run() {
-                 textView.setText(Integer.toString(Counter));
-                 Counter--;
+                 final TextView textView = findViewById(R.id.textView);
+                 textView.setText(Integer.toString(counter));
+                 //Reset the counter
+                 if (counter == 0) {
+                     counter = 99;
+                 }
+                 counter--;
                  handler.postDelayed(this, 1000);
 
              }
@@ -29,4 +46,3 @@ public class MainActivity extends AppCompatActivity {
 
      }
 }
-
